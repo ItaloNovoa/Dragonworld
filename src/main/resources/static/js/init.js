83909b274d82bc2b80de16c68f720057cb8d549a
acai-bjca <amalia.alfonso@mail.escuelaing.edu.co>
2019-04-09 01:08:53 -0500

    revert
    Co-authored-by: Siabell <valesiabatto@gmail.com>

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -1,227 +1,177 @@
-//var stompClient = null;
-
 var SceneA = new Phaser.Class({
-	Extends: Phaser.Scene,
-	initialize:
-		function SceneB() {
-		Phaser.Scene.call(this, 'sceneB');
-		this.graphics;
-		this.timerEvent;
-		this.clockSize = 240;
-	},
+    Extends: Phaser.Scene,
+    initialize:
+        function SceneB() {
+            Phaser.Scene.call(this, 'sceneA');
+            this.fondoInicio;
+        },
 
-	preload: function () {
-		this.load.image('fondo', 'images/fondo5.jpg');
-		this.load.image('drag', 'images/dragon3.png');
-		this.load.image('food', 'images/star.png');
-		this.load.atlas('dragones', 'images/dragones.png', 'images/dragones.js');
-	},
+    preload: function () {
+        this.load.image('fInicio', 'images/fondo4.jpg');
+        this.load.image('block', 'images/50x50-black.png');
+    },
 
-	create: function () {
-		this.add.image(config.width / 2, config.height / 2, 'fondo');
-	},
-	preload: preload,
-	create: create,
-	update: update
+    create: function () {
+        //this.fondoInicio = this.add.image(config.width / 2, config.height / 2, 'fInicio');
+        this.add.image(config.width / 2, config.height / 2, 'fInicio');
+        var blocks = this.add.group({ key: 'block', repeat: 210 });
+        this.input.once('pointerdown', function () {
+            this.scene.switch('sceneB');
+        }, this);
+
+    }
+});
+
+var SceneB = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+        function SceneB() {
+            Phaser.Scene.call(this, 'sceneB');
+            this.graphics;
+            this.timerEvent;
+            this.clockSize = 240;
+        },
+
+    preload: function () {
+        this.load.image('fondo', 'images/fondo5.jpg');
+        this.load.image('drag', 'images/dragon3.png');
+        this.load.image('food', 'images/star.png');
+    },
+
+    create: function () {
+        this.add.image(config.width / 2, config.height / 2, 'fondo');
+    },
+    preload: preload,
+    create: create,
+    update: update
 });
 
 const config = {
-		type: Phaser.AUTO,
-		width: document.documentElement.clientWidth - 20,
-		height: document.documentElement.clientHeight - 20,
-		backgroundColor: '#000000',
-		parent: 'container',
-		scene: [SceneA],
-		physics: {
-		default: "arcade",
-		arcade: {
-			}
-		},
+    type: Phaser.AUTO,
+    width: document.documentElement.clientWidth - 20,
+    height: document.documentElement.clientHeight - 20,
+    backgroundColor: '#000000',
+    parent: 'container',
+    scene: [SceneA, SceneB],
+    physics: {
+        default: "arcade",
+        arcade: {
+            
+        }
+    },
 };
 
 var game = new Phaser.Game(config);
 var score = 0;
 var scoreText;
 var foods;
-var activo;
 
 function preload() {
-	this.load.image('fondo', 'images/fondo5.jpg');
-	this.load.image('drag', 'images/dragon3.png');
-	this.load.image('food', 'images/esfera3.png');
-	this.load.atlas('dragones', 'images/dragones.png', 'images/dragones.js');
-	this.load.atlas('fuegos', 'images/fuego1.png', 'images/fuego.js');
+    this.load.image('fondo', 'images/fondo5.jpg');
+    this.load.image('drag', 'images/dragon3.png');
+    this.load.image('food', 'images/esfera3.png');
+    this.load.atlas('dragones', 'images/dragones.png', 'images/dragones.js');
+    this.load.atlas('fuegos', 'images/fuego4.png', 'images/fuego.js');
 
 }
 function create() {
-	this.foods =this.physics.add.group();
-	this.listaDragones =this.physics.add.group();
-
-	/*Fondo*/
-	this.fondo = this.add.image((config.width / 2)-100, (config.height / 2)-100, 'fondo');
-	this.fondo.setScale(1.3);
-
-	/*Dragones*/
-	this.anims.create({ key: 'dragon1', frames: this.anims.generateFrameNames('dragones', { prefix: 'dragon1_', end: 100, zeroPad: 4 }), repeat: -1 });
-	this. fuegoSprite = this.anims.create({ key: 'fuego1', frames: this.anims.generateFrameNames('fuegos', { prefix: 'fuego_', end: 100, zeroPad: 4 }), repeat: 0 });	
-	this.dragon = this.physics.add.sprite(400, 100, 'dragones').play('dragon1');  
-	this.activo=true;
-	this.dragon.setCollideWorldBounds(true);
-	this.physics.add.collider(this.dragon, this.foods, collectFood, null, this);
-	this.physics.add.overlap(this.dragon, this.foods, collectFood, null, this);
-	this.input.on('pointerdown', function (pointer) {
-		if(this.activo==true){
-			this.activo=false;
-			this.fuegoSprite = this.add.sprite(this.dragon.x, this.dragon.y, 'fuegos').play('fuego1');
-			let cursor = pointer;
-			//calcular el angulo entre el dragon y el mouse (tomando la esquina del sprite (--arreglar eso))
-			let angleNow = (Math.atan2(this.dragon.y - cursor.y, this.dragon.x - cursor.x) * 180 / Math.PI);
-			this.fuegoSprite.angle = angleNow-180;
-			var time;			
-			timedEvent = this.time.delayedCall(250, onEvent, [], this);
-		}
-	}, this);
-
-	/*Comida*/
-	for (var i = 0; i < 20; i++){
+    this.fondo = this.add.image(config.width / 2, config.height / 2, 'fondo');
+    this.anims.create({ key: 'dragon1', frames: this.anims.generateFrameNames('dragones', { prefix: 'dragon1_', end: 100, zeroPad: 4 }), repeat: -1 });
+    this. f = this.anims.create({ key: 'fuego1', frames: this.anims.generateFrameNames('fuegos', { prefix: 'fuego_', end: 100, zeroPad: 4 }), repeat: 0 });
+    this.dragon = this.physics.add.sprite(400, 100, 'dragones').play('dragon1');      
+    this.foods =this.physics.add.group();
+    for (var i = 0; i < 20; i++)
+	{
 		this.foods.create(Phaser.Math.FloatBetween(32, config.width - 32),Phaser.Math.FloatBetween(32, config.height - 32), 'food');
 	}
-	
-	/*Puntaje*/
-	scoreText = this.add.text(150, 40, 'score: 0', { fontSize: '32px', fill: '#000' });	
-	this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);	
+   
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.dragon.setCollideWorldBounds(true);
+
+    this.physics.add.collider(this.dragon, this.foods, collectFood, null, this);
+    this.physics.add.overlap(this.dragon, this.foods, collectFood, null, this);
+    
+    this.input.on('pointerdown', function (pointer) {
+        this.f = this.add.sprite(this.dragon.x, this.dragon.y, 'fuegos').play('fuego1');
+        let cursor = pointer;        
+        //calcular el angulo entre el dragon y el mouse (tomando la esquina del sprite (--arreglar eso))
+        let angleNow = (Math.atan2(this.dragon.y - cursor.y, this.dragon.x - cursor.x) * 180 / Math.PI);
+        this.f.angle = angleNow-180;
+        var time;
+        //this.f.events.onInputDown.add(destroySprite, this);
+        //this.f.time.destroy(1000);
+        //this.f.destroy(2000);
+        //event.gameObject.setActive(false);
+        //
+        //console.log(game);
+        
+        //this.add.tween(this.f).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+        /**var timedEvent = this.time.addEvent({
+            delay: 5000,
+            callback: fadePicture(this.f),
+            callbackScope:this,
+        });*/
+        timedEvent = this.time.delayedCall(250, onEvent, [], this);
+    }, this);
 }
 
 function onEvent (){
-	console.log("as")
-	this.fuegoSprite.setActive(false);
-	this.fuegoSprite.setVisible(false);
-	this.activo=true;
+    console.log("as")
+    this.f.setActive(false);
+    this.f.setVisible(false); 
 }
 
-function creteDragon(){
-	dragon2 = this.physics.add.sprite(400, 100, 'dragones').play('dragon1');
-	this.listaDragones.create(400, 100, dragon2);
-}
+function update(time, delta) {    
+    if (this.space.isUp) {
+        this.dragon.setVelocity(0, 0);
+    }
+    if (this.input.mousePointer.x == this.dragon.x && this.input.mousePointer.y == this.dragon.y) {
+        this.dragon.setVelocity(0, 0);
+    }
+    //movimiento que siga el mouse 
+    this.physics.moveTo(this.dragon, this.input.mousePointer.x, this.input.mousePointer.y + 42, 250);
+    
+     this.input.on('pointermove', function (pointer) {
+        let cursor = pointer;
+        //calcular el angulo entre el dragon y el mouse (tomando la esquina del sprite (--arreglar eso))
+        let angleNow = (Math.atan2(this.dragon.y - cursor.y, this.dragon.x - cursor.x) * 180 / Math.PI);
+        this.dragon.angle = angleNow;
+    }, this);
+    /*
+      this.input.off('pointermove', function (pointer) {
+          let cursor = pointer;
+          this.physics.moveTo(this.dragon, cursor.x, cursor.y,100);
+          let angle = Phaser.Math.Angle.Between(this.dragon.x, this.dragon.y, cursor.x + this.cameras.main.scrollX, cursor.y + this.cameras.main.scrollY)
+      }, this);*/
 
-function update(time, delta) {
-	if (this.space.isUp) {
-		this.dragon.setVelocity(0, 0);
-	}
-	if (this.input.mousePointer.x == this.dragon.x && this.input.mousePointer.y == this.dragon.y) {
-		this.dragon.setVelocity(0, 0);
-	}
-	//movimiento del dragon que siga el mouse
-	this.physics.moveTo(this.dragon, this.input.mousePointer.x, this.input.mousePointer.y + 32, 200);
-
-	//stompClient.send("/topic/dragon", {}, JSON.stringify(this.dragon.x));
-	this.input.on('pointermove', function (pointer) {
-		let cursor = pointer;
-		//calcular el angulo entre el dragon y el mouse (tomando la esquina del sprite (--arreglar eso))
-		let angleNow = (Math.atan2(this.dragon.y - cursor.y, this.dragon.x - cursor.x) * 180 / Math.PI);
-		this.dragon.angle = angleNow;
-	}, this);
 }
 
 function collectFood(dragon, food) {
-	food.disableBody(true, true);
-	this.foods.remove(food)
-	score += 10;
-	scoreText.setText('Score: ' + score);
-	// si se acaba la comida, se vuelve a regenerar
-	if (this.foods.children.entries.length <= 19) {
-		this.foods.create(Phaser.Math.FloatBetween(150, config.width - 150),Phaser.Math.FloatBetween(100, config.height - 100), 'food');
-	}
+    food.disableBody(true, true);
+    this.foods.remove(food)
+    score += 10;
+    scoreText.setText('Score: ' + score);
+    // si se acaba la comida, se vuelve a regenerar
+    if (this.foods.children.entries.length <= 19) {
+        this.foods.create(Phaser.Math.FloatBetween(32, config.width - 32),Phaser.Math.FloatBetween(32, config.height - 32), 'food');
+    }
 }
 
 var animateButton = function (e) {
-	e.preventDefault;	
-	e.target.classList.remove('animate');
 
-	e.target.classList.add('animate');
-	setTimeout(function () {
-		e.target.classList.remove('animate');
-	}, 700);
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function () {
+        e.target.classList.remove('animate');
+    }, 700);
 };
 
 var bubblyButtons = document.getElementsByClassName("bubbly-button");
 
 for (var i = 0; i < bubblyButtons.length; i++) {
-	bubblyButtons[i].addEventListener('click', animateButton, false);
+    bubblyButtons[i].addEventListener('click', animateButton, false);
 }
-
-/***************************************************** STOMP ********************************************************************/
-
-var app = (function () {
-
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
-        }        
-	}    
-	
-    var addPointToCanvas = function (point) {        
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
-        //ctx.arc(point.x, point.y);
-        ctx.stroke();
-    };
-
-    var connectAndSubscribe = function () {
-		console.info('Connecting to WS...');
-		var socket = new SockJS('/stompDragon');
-		stompClient = Stomp.over(socket);	
-		stompClient.connect({}, function (frame) {            
-			console.log('Connected: ' + frame);
-			stompClient.subscribe('/topic/dragon', function (eventbody) {
-				var puntoJSON =  JSON.parse(eventbody.body);
-				//var callback = mostrarMensaje;
-				var callback = addPointToCanvas;
-				mostrar(puntoJSON, callback);
-			});
-		});
-	
-		stompClient.connect({}, function (frame) {            
-			console.log('Connected: ' + frame);
-			var sala = document.getElementById("sala").value;     
-			stompClient.subscribe('/topic/dragon/'+sala, function (eventbody) {
-				var puntoJSON =  JSON.parse(eventbody.body);
-				var callback = mostrarMensaje;
-				//var callback = addPointToCanvas;
-				mostrar(puntoJSON, callback);
-			});            
-		});
-	};
-
-    function mostrar(puntoJSON, callback){
-        callback(puntoJSON);
-    }
-    
-    function mostrarMensaje(puntoJSON){
-        alert("Coordenada X: " + puntoJSON.x + ", Coordenada Y: "+ puntoJSON.y);
-    }   
-
-    return {
-        init: function () {
-			var can = document.getElementById("canvas");       
-            //websocket connection
-            connectAndSubscribe();
-        },
-        
-        disconnect: function () {
-            if (stompClient !== null) {
-                stompClient.disconnect();
-            }
-            setConnected(false);
-            console.log("Disconnected");
-        },
-
-        connectTopic: function(){
-            connectAndSubscribe();
-        }
-    };
-
-})();
